fd2e491b1882f8081e5647dba5c5ad25f47f2911
Ann Guinee <annmguinee@gmail.com>
2021-01-16 21:48:47 -0800

    Server initated and sequelize initiated

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require("cors")
+const compression = require("compression");
+
+const PORT = process.env.PORT || 8080
+
+
+const app = express()
+
+
+app.use(cors())
+app.use(express.urlencoded({ limit: '200mb',extended: true }));
+app.use(express.json({ limit: '200mb',extended: true }));
+app.use(compression());
+
+app.use(express.static("public"));
+
+app.listen(PORT, function () {
+    console.log("Server listening on: http://localhost:" + PORT);
+})
